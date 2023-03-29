5005c14ceff08ca1deeb976da703708087d3c882
mosafari <m.safari2680@gmail.com>
2023-03-29 14:42:58 +0430

    add spring project

---
+++ toptencoin/src/main/java/com/javap/toptencoin/ToptencoinApplication.java
@@ -0,0 +1,13 @@
+package com.javap.toptencoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToptencoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToptencoinApplication.class, args);
+	}
+
+}
---
+++ toptencoin/src/test/java/com/javap/toptencoin/ToptencoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javap.toptencoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToptencoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
